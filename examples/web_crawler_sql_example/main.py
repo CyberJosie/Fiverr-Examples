08998ab0fcefef2b6302cf6bb0eac23b1945d016
Jocelyn <107489279+CyberJosie@users.noreply.github.com>
2022-07-26 10:25:54 -0700

    Update main.py

--- examples/web_crawler_sql_example/main.py
+++ examples/web_crawler_sql_example/main.py
@@ -1,4 +1,11 @@
 from src.Crawler import init
 
+"""
+Whiskey Web Crawler
+
+Retrives feautured products, top selling products, and new products from https://www.whiskyshopusa.com/
+and saves to SQL database
+"""
+
 if __name__ == "__main__":
     init()
